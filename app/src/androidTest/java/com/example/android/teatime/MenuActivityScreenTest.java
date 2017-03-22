0e6c8e7d236df60a0e52dff598490abf71be6543
jlin27 <jessica.lin@udacity.com>
2017-03-21 17:18:01 -0700

    TESP.02-Exercise-AddMenuActivityScreenTest

---
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -0,0 +1,41 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.example.android.teatime;
+
+/**
+ * This test demos a user clicking on a GridView item in MenuActivity which opens up the
+ * corresponding OrderActivity.
+ *
+ * This test does not utilize Idling Resources yet. If idling is set in the MenuActivity,
+ * then this test will fail. See the IdlingResourcesTest for an identical test that
+ * takes into account Idling Resources.
+ */
+
+
+// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+public class MenuActivityScreenTest {
+
+    // TODO (2) Add the rule that provides functional testing of a single activity
+
+    // TODO (3) Finish writing this test which will click on a gridView Tea item and verify that
+    // the OrderActivity opens up with the correct tea name displayed.
+
+    public void clickGridViewItem_OpensOrderActivity() {
+
+    }
+
+}
