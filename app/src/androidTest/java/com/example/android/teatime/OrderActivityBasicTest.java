79b5a8e29590c8f5fc0a1b383545fcb899c5596a
jlin27 <jessica.lin@udacity.com>
2017-03-21 17:16:29 -0700

    TESP.01-Solution-FinishOrderActivityBasicTest

--- app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -16,21 +16,55 @@
 
 package com.example.android.teatime;
 
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 /**
  * This test demos a user clicking the decrement button and verifying that it properly decrease
  * the quantity the total cost.
  */
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class OrderActivityBasicTest {
 
-    // TODO (2) Add the rule that provides functional testing of a single activity
+    /**
+     * The ActivityTestRule is a rule provided by Android used for functional testing of a single
+     * activity. The activity that will be tested will be launched before each test that's annotated
+     * with @Test and before methods annotated with @before. The activity will be terminated after
+     * the test and methods annotated with @After are complete. This rule allows you to directly
+     * access the activity during the test.
+     */
 
-    // TODO (3) Finish writing this test which will:
-    //          - Check that the initial quantity is zero
-    //          - Click on the decrement button
-    //          - Verify that the decrement button won't decrease the quantity 0 and cost below $0.00
+    @Rule
+    public ActivityTestRule<OrderActivity> mActivityTestRule =
+            new ActivityTestRule<>(OrderActivity.class);
 
+    @Test
     public void clickDecrementButton_ChangesQuantityAndCost() {
+
+        // Check the initial quantity variable is zero
+        onView((withId(R.id.quantity_text_view))).check(matches(withText("0")));
+
+        // Click on decrement button
+        onView((withId(R.id.decrement_button)))
+                .perform(click());
+
+        // Verify that the decrement button decreases the quantity by 1
+        onView(withId(R.id.quantity_text_view)).check(matches(withText("0")));
+
+        // Verify that the increment button also increases the total cost to $5.00
+        onView(withId(R.id.cost_text_view)).check(matches(withText("$0.00")));
+
     }
 }
