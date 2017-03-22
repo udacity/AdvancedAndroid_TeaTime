fad5e849d0700788f3121a84dacd4fd383c26958
jlin27 <jessica.lin@udacity.com>
2017-03-21 17:14:23 -0700

    TESP.01-Exercise-FinishOrderActivityBasicTest

---
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -0,0 +1,36 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.example.android.teatime;
+
+/**
+ * This test demos a user clicking the decrement button and verifying that it properly decrease
+ * the quantity the total cost.
+ */
+
+// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+public class OrderActivityBasicTest {
+
+    // TODO (2) Add the rule that provides functional testing of a single activity
+
+    // TODO (3) Finish writing this test which will:
+    //          - Check that the initial quantity is zero
+    //          - Click on the decrement button
+    //          - Verify that the decrement button won't decrease the quantity 0 and cost below $0.00
+
+    public void clickDecrementButton_ChangesQuantityAndCost() {
+    }
+}
