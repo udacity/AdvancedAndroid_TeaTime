b30475c0f08968e255e6e12d73f5ac863ee2dc34
jlin27 <jessica.lin@udacity.com>
2017-03-21 17:19:10 -0700

    TESP.02-Solution-AddMenuActivityScreenTest

--- app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -16,6 +16,23 @@
 
 package com.example.android.teatime;
 
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.Matchers.anything;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
  * corresponding OrderActivity.
@@ -26,15 +43,34 @@ package com.example.android.teatime;
  */
 
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class MenuActivityScreenTest {
 
-    // TODO (2) Add the rule that provides functional testing of a single activity
+    public static final String TEA_NAME = "Green Tea";
 
-    // TODO (3) Finish writing this test which will click on a gridView Tea item and verify that
-    // the OrderActivity opens up with the correct tea name displayed.
+    /**
+     * The ActivityTestRule is a rule provided by Android used for functional testing of a single
+     * activity. The activity that will be tested will be launched before each test that's annotated
+     * with @Test and before methods annotated with @Before. The activity will be terminated after
+     * the test and methods annotated with @After are complete. This rule allows you to directly
+     * access the activity during the test.
+     */
+    @Rule
+    public ActivityTestRule<MenuActivity> mActivityTestRule = new ActivityTestRule<>(MenuActivity.class);
 
+    /**
+     * Clicks on a GridView item and checks it opens up the OrderActivity with the correct details.
+     */
+    @Test
     public void clickGridViewItem_OpensOrderActivity() {
+
+        // Uses {@link Espresso#onData(org.hamcrest.Matcher)} to get a reference to a specific
+        // gridview item and clicks it.
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(1).perform(click());
+
+        // Checks that the OrderActivity opens with the correct tea name displayed
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText(TEA_NAME)));
+
 
     }
 
