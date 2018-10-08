5b53831ac7e064d041e03b113eb3f82b1044a6b8
burakozknn <burakozknn@gmail.com>
2018-10-08 02:02:12 -0400

    updated AddOrderSummaryActivityTest

--- app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -16,24 +16,79 @@
 
 package com.example.android.teatime;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCategories;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
+
 // TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class OrderSummaryActivityTest {
 
+    private static final String emailMessage = "I just ordered a delicious tea from TeaTime. Next time you are craving a tea, check them out!";
+
     // TODO (2) Add the rule that indicates we want to use Espresso-Intents APIs in functional UI tests
+
+    @Rule
+    public IntentsTestRule<OrderSummaryActivity> mActivityRule = new IntentsTestRule<>(
+            OrderSummaryActivity.class);
 
 
     // TODO (3) Finish this method which runs before each test and will stub all external
     // intents so all external intents will be blocked
 
+    @Before
     public void stubAllExternalIntents() {
-
+        // By default Espresso Intents does not stub any Intents. Stubbing needs to be setup before
+        // every test run. In this case all external Intents will be blocked.
+        intending(not(isInternal())).respondWith(new Instrumentation.ActivityResult( Activity.RESULT_OK, null));
     }
+
+
 
 
     // TODO (4) Finish this method which verifies that the intent sent by clicking the send email
     // button matches the intent sent by the application
 
+    @Test
     public void clickSendEmailButton_SendsEmail() {
 
+        onView((withId(R.id.send_email_button))).perform( click() );
+
+        intended(allOf(
+                hasAction( Intent.ACTION_SENDTO),
+                hasExtra( Intent.EXTRA_TEXT,emailMessage )));
+
+
+
+
+
     }
+
+
 }
